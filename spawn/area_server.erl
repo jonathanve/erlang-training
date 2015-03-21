c88df35255b926e0f6973529c85c0ccb239123b0
Jonathan Valencia <jonathanv.espitia@gmail.com>
2015-03-21 14:33:52 -0500

    concurrency

---
+++ spawn/area_server.erl
@@ -0,0 +1,38 @@
+% client-server model
+
+-module(area_server).
+-export([start/0, area/2, loop/0]).
+
+start() ->
+    spawn(?MODULE, loop, []).
+
+area(Pid, What) ->
+    rpc(Pid, What).
+
+% this will take max 5 secs to respond
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+	{Pid, Response} ->
+	    Response
+    after 5000 ->
+        {error, timeout}
+    end.
+
+loop() ->
+    receive
+	{From, {rectangle, Width, Height}} ->
+	    From ! {self(), Width * Height};
+	{From, {circle, R}} ->
+	    From ! {self(), 3.14159 * R * R};
+	{From, Other} ->
+            sleep(10000),
+	    From ! {self(), {error, Other}}
+    end,
+    loop().
+
+sleep(T) ->
+    receive
+    after T ->
+        true
+    end.
---
+++ spawn/ctemplate.erl
@@ -0,0 +1,19 @@
+-module(ctemplate).
+-compile(export_all).
+
+start() ->
+    spawn(?MODULE, loop, []).
+
+rpc(Pid, Request) ->   
+    Pid ! {self(), Request},
+     receive
+	 {Pid, Response} ->
+	     Response
+     end.
+
+loop() ->    
+    receive
+	{From, Any} ->
+	    From ! {self(), io:format("Received:~p~n",[Any])},
+	    loop()
+    end.
