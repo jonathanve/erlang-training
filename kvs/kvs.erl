ab81f6aa68197b171144f538e5649e1ee7c9e7bb
Jonathan Valencia <jonathanv.espitia@gmail.com>
2015-03-22 13:05:10 -0500

    distributed erl base case

---
+++ kvs/kvs.erl
@@ -0,0 +1,29 @@
+-module(kvs).
+-export([start/0, lookup/1, store/2]).
+
+start() ->
+    register(kvs, spawn(fun() -> loop() end)).
+
+lookup(Key) ->
+    rpc({lookup, Key}).
+
+store(Key, Value) ->
+    rpc({store, Key, Value}).
+
+rpc(Q) ->
+    kvs ! {self(), Q},
+    receive
+	{kvs, Reply} ->
+	    Reply
+    end.
+
+loop() ->
+    receive
+	{From, {store, Key, Value}} ->
+		put(Key, {ok, Value}),
+		From ! {kvs, true},
+		loop();
+	{From, {lookup, Key}} ->
+		From ! {kvs, get(Key)},
+		loop()
+    end.
